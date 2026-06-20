aef0269a9518176677dfbf5a09872312e89d96ac
Fabrício Barros Cabral <fabriciofx@gmail.com>
2026-06-20 18:17:04 -0300

    feat: Add install dependencies on deploy

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,12 @@ npm run build && cp -a dist/. "$DIR/frontend/"
 echo "done."
 
 echo -n "Deploying backend files to $DIR/backend... "
-cd server && npm run build && cp -a dist/. "$DIR/backend/"
+cd server && npm run build && \
+  cp -a dist/. "$DIR/backend/" && \
+  cp package.json "$DIR/backend/" && \
+  cp package-lock.json "$DIR/backend/"
+echo "done."
+
+echo -n "Installing backend dependencies in $DIR/backend... "
+cd "$DIR/backend" && npm install --omit=dev
 echo "done."
